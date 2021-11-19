d5468c2e45cada212f39508c932e6cc8a4ddfae1
Женя Вито z z z <evgenizef.ai@gmail.com>
2021-11-19 21:59:41 +1000

    поменял комменты

---
+++ UdpServerVer-1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UdpServerVer_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8081;
+
+            var udpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            udpSocket.Bind(udpEndPoint);
+
+            while (true)
+            {
+                var bufer = new byte[256];
+                var size = 0;
+                var data = new StringBuilder();
+                EndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                do
+                {
+                    size = udpSocket.ReceiveFrom(bufer, ref senderEndPoint);
+                    data.Append(Encoding.UTF8.GetString(bufer));
+                }
+                while (udpSocket.Available > 0);
+
+                udpSocket.SendTo(Encoding.UTF8.GetBytes("Сообщение отправлено"), senderEndPoint);
+
+                Console.WriteLine(data);
+
+                // выход из цикла нужно ли?
+            }
+            // закрытие сокета нужно?
+        }
+    }
+}
---
+++ udpClient/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace udpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8082;
+
+            var udpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            udpSocket.Bind(udpEndPoint);
+
+            while (true)
+            {
+                Console.WriteLine("Введите сообщение");
+                var messeg = Console.ReadLine();
+
+                var serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8081);
+                udpSocket.SendTo(Encoding.UTF8.GetBytes(messeg), serverEndPoint);
+
+                var bufer = new byte[256];
+                var size = 0;
+                var data = new StringBuilder();
+                EndPoint senderEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8081);
+
+                do
+                {
+                    size = udpSocket.ReceiveFrom(bufer, ref senderEndPoint);
+                    data.Append(Encoding.UTF8.GetString(bufer));
+                }
+                while (udpSocket.Available > 0);
+
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
